85615d2a964fbdac436002124921c4f32341fc15
RahmaOktoyadi19 <rahmaoktoyadi37f@gmail.com>
2025-11-09 20:50:07 +0700

    Add files via upload

---
+++ sisteminformasiklinikkampus/dataJanjiTemu.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sisteminformasiklinikkampus;
+
+/**
+ *
+ * @author YADI
+ */
+public class dataJanjiTemu {
+    public static String dokter = "Dr. Smith";
+    public static String tanggal = "Nov 8, 2025";
+}
